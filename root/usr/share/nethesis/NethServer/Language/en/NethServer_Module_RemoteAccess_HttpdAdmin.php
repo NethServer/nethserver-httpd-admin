6a08545074972d9a78127f8277a2df4be5a626aa
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2012-11-27 18:02:38 +0100

    Translation: add missing english and italian translation #523

--- root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_RemoteAccess_HttpdAdmin.php
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_RemoteAccess_HttpdAdmin.php
@@ -3,5 +3,5 @@
 $L['HttpdAdmin_Title'] = 'Web interface';
 $L['NethServer\Module\RemoteAccess_RemoteManagement_caption'] = 'Remote management';
 
-
+$L['Authorize network'] = 'Authorize network';
 
--- root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_RemoteAccess_HttpdAdmin.php
+++ root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_RemoteAccess_HttpdAdmin.php
@@ -4,4 +4,4 @@ $L['HttpdAdmin_Title'] = 'Interfaccia web';
 
 $L['NethServer\Module\RemoteAccess_RemoteManagement_caption'] = 'Gestione remota';
 
-
+$L['Authorize network'] = 'Autorizza rete';
