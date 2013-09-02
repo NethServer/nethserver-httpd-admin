1ca852bdb511a86797dce80ee5a270254708b340
Davide Principi <davide.principi@nethesis.it>
2013-09-02 16:56:16 +0200

    index_dev.php: load default "system" modules. Refs #2113

--- root/usr/share/nethesis/nethserver-manager/index_dev.php
+++ root/usr/share/nethesis/nethserver-manager/index_dev.php
@@ -60,8 +60,21 @@ $FW
 ;
 
 try {
-    $FW->dispatch($FW->createRequest());
+    $R = $FW->createRequest();
+    if ($R->getFormat() === 'xhtml') {
+        $R->setParameter('Menu', array())
+            ->setParameter('Notification', array())
+            ->setParameter('Resource', array())
+            ->setParameter('Logout', array())
+        ;
+    } elseif ($R->getFormat() === 'json') {
+        $R->setParameter('Notification', array());
+    }
+    $FW->dispatch($R);
 } catch (\Nethgui\Exception\HttpException $ex) {
     $FW->printHttpException($ex, TRUE);
+} catch (\Exception $ex) {
+    $FW->printHttpException(new Nethgui\Exception\HttpException('Internal server error', 500, 1377609335, $ex), TRUE);
 }
+
 
