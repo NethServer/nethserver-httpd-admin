06ec2c3a907bfd202a29dd59e6431d7b55213e14
Davide Principi <davide.principi@nethesis.it>
2016-07-04 10:51:40 +0200

    Removed qtip inclusion

--- root/usr/share/nethesis/NethServer/Template/NethServer.php
+++ root/usr/share/nethesis/NethServer/Template/NethServer.php
@@ -79,7 +79,6 @@ if (defined('NETHGUI_DEBUG') && NETHGUI_DEBUG === TRUE) {
  * jQuery plugins
  */
 $globalUseFile->append('js/jquery.dataTables.min.js');
-$globalUseFile->append('js/jquery.qtip.min.js');
 
 $lang = substr($view->getTranslator()->getLanguageCode(), 0, 2);
 if ($lang !== 'en') {
@@ -93,7 +92,6 @@ $view
     ->includeJavascript($bootstrapJs)
     // CSS:
     ->useFile('css/ui/jquery-ui-1.8.16.custom.css')
-    ->useFile('css/jquery.qtip.min.css')
     ->useFile('css/font-awesome.css')
     ->useFile('css/base.css')
 ;
