6a1159757d717e9fb4a4c0ade3d9e8cf11037b95
Davide Principi <davide.principi@nethesis.it>
2019-09-17 15:50:47 +0200

    Automatically fix URL scheme
    As PHP is lacking HTTP_HOST in the DocumentError page, we generate the
    page on the client side with the correct hostname (the one typed to
    reach the server).
    
    This works also with port forwards.

--- nethserver-manager/error.php
+++ nethserver-manager/error.php
@@ -1,19 +1,31 @@
 <?php
     header('Content-type:text/html; charset: utf-8');
     $url = htmlspecialchars(sprintf("https://%s:980", $_SERVER['SERVER_ADDR']));
+    $url2 = htmlspecialchars(sprintf("https://%s:980", $_SERVER['SERVER_NAME']));
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
 <title>Bad request</title>
+<noscript>
 <meta http-equiv="refresh" content="2; URL='<?php echo $url ?>'">
+</noscript>
+<script type="text/javascript">
+var url = window.location.href.replace("http://", "https://");
+document.write('<meta http-equiv="refresh" content="2; URL=\'' + url + '\'">');
+</script>
 </head>
 <body>
 <h1>Bad request</h1>
-<p>Your browser sent a request that this server could not understand.<br>
-Reason: You're speaking plain HTTP to an SSL-enabled server port.<br>
-Instead use the HTTPS scheme to access this URL, please.</p>
-<a href="<?php echo $url ?>"><?php echo $url ?></a>
+<p>Use the HTTPS scheme to access this URL!<p>
+<p>Please try with
+<noscript>
+<a href="<?php echo $url2 ?>"><?php echo $url2 ?></a>
+</noscript>
+<script type="text/javascript">
+var url = window.location.href.replace("http://", "https://");
+document.write('<a href="' + url + '">' + url + '</a>');
+</script>
+</p>
 </body>
 </html>
-
 
